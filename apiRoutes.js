120fe6a33016f8dde088974a92cb2dead82b7e20
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2021-04-29 22:48:01 -0500

    Updated script to save notes

--- apiRoutes.js
+++ apiRoutes.js
@@ -7,19 +7,32 @@ module.exports = (app) => {
     app.post('/api/notes', (req, res) => {
         const newNote = req.body;
         console.log(newNote);
+        if (storedNotes.length >0){
+        newNote.id = storedNotes[storedNotes.length-1].id + 1;}
+        else {newNote.id = 0};
         storedNotes.push(newNote);
         writeToFile('db/db.json', JSON.stringify(storedNotes));
         res.json(storedNotes);
     })
 
-    app.delete(`/api/notes/${id}`, (req, res) => {
-        storedNotes.delete(`${id}`);
+    app.delete('/api/notes/:id', (req, res) => {
+        const removal = req.params.id
+        for (let index = 0; index < storedNotes.length; index++) {
+            const removed = storedNotes[index];
+            
+            if (removed.id == removal){
+                console.log(removed);
+                storedNotes.splice(index, 1);
+                break; 
+            }
+        } console.log(storedNotes);
+        writeToFile('db/db.json', JSON.stringify(storedNotes));
+        res.json(storedNotes);
     })
 }
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName,data, function(err){
         if (err) return console.log(err);
-        console.log(data);
     })
 }
