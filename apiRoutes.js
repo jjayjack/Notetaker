e89579cef96a8d9d40cba7bf8caa68effe8b3c4c
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2021-04-29 19:34:18 -0500

    Updated code to collect user input

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,10 +1,25 @@
-const storedNotes = require('./db/db.json');
+const storedNotes = require('./db/db');
+const fs = require('fs');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(storedNotes));
+
     app.post('/api/notes', (req, res) => {
         const newNote = req.body;
+        console.log(newNote);
         storedNotes.push(newNote);
-        res.json(newNote);
+        writeToFile('db/db.json', JSON.stringify(storedNotes));
+        res.json(storedNotes);
+    })
+
+    app.delete(`/api/notes/${id}`, (req, res) => {
+        storedNotes.delete(`${id}`);
+    })
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data, function(err){
+        if (err) return console.log(err);
+        console.log(data);
     })
 }
